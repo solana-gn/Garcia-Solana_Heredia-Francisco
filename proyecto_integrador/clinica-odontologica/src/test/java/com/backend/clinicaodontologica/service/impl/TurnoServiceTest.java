ac2c0dd243147d1dd6621e141a8e6e2c278c608f
solana-gn <garcianavarrosolana@gmail.com>
2023-11-30 21:14:02 -0300

    Primer test unitario de Turnos OK

--- proyecto_integrador/clinica-odontologica/src/test/java/com/backend/clinicaodontologica/service/impl/TurnoServiceTest.java
+++ proyecto_integrador/clinica-odontologica/src/test/java/com/backend/clinicaodontologica/service/impl/TurnoServiceTest.java
@@ -1,11 +1,33 @@
 package com.backend.clinicaodontologica.service.impl;
 
-
+import com.backend.clinicaodontologica.dto.salida.paciente.PacienteSalidaDto;
+import com.backend.clinicaodontologica.dto.salida.turno.TurnoSalidaDto;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
 public class TurnoServiceTest {
+
+    @Autowired
+    private TurnoService turnoService;
+
+    @Test
+    @Order(1)
+    void deberiaDevolverUnaListaVaciaDeTurnos(){
+
+        List<TurnoSalidaDto> turnosDto = turnoService.listarTurnos();
+
+        assertTrue(turnosDto.isEmpty());
+
+    }
+
 
 
 }
